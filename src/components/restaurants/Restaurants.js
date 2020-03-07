1a877f2720c60d4965719c43bc08ff2c123966c5
nbert100 <nbert14@gmail.com>
2020-03-06 20:58:49 -0500

    Done.

--- src/components/restaurants/Restaurants.js
+++ src/components/restaurants/Restaurants.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
+import Restaurant from './Restaurant'
 
 class Restaurants extends Component {
   render() {
+
+    const restaurants = this.props.restaurants.map((restaurant) => <Restaurant key={this.props.id} {...restaurant} />)
     return(
+      <div>
       <ul>
-        Restaurants Component
+        { restaurants }
       </ul>
+      </div>
     );
   }
 };
+
+// const Restaurants = props => {
+  
+//   return (
+//     <div>
+//       { restaurants }
+//     </div>
+//   )
+// }
 
 export default Restaurants;
--- src/reducers/manageRestaurant.js
+++ src/reducers/manageRestaurant.js
@@ -2,6 +2,17 @@
 import cuid from 'cuid';
 export const cuidFn = cuid;
 
-export default function manageRestaurants(state, action) {
+export default function manageRestaurants(state = { restaurants: [] }, action) {
+    switch (action.type) {
+        case 'ADD_RESTAURANT':
+            const restaurant ={
+                id: action.cuid, text: action.text
+            }
+            return { ...state, restaurants: [...state.restaurants, restaurant] }
+
+            default:
+                return state;
+    }
+    
 
 }
